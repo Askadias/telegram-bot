5a1d38a83463d18c3ca70a01abc41318e144ff92
v-uprykhodzka <v-uprykhodzka@expedia.com>
2019-06-22 14:51:29 +0300

    Moved to better framework

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
+import logging
 from os import environ
 
-from telebot import TeleBot
+from telegram.error import (TelegramError, Unauthorized, BadRequest,
+                            TimedOut, ChatMigrated, NetworkError)
+from telegram.ext import Updater, CommandHandler
 
-app = TeleBot(__name__)
+updater = Updater(token=environ.get('API_KEY'), use_context=True)
+dispatcher = updater.dispatcher
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-@app.route('/command ?(.*)')
-def example_command(message, cmd):
-    chat_dest = message['chat']['id']
-    msg = "Command Recieved: {}".format(cmd)
-
-    app.send_message(chat_dest, msg)
-
-
-@app.route('(?!/).+')
-def parrot(message):
-    chat_dest = message['chat']['id']
-    user_msg = message['text']
-
-    msg = "Parrot Says: {}".format(user_msg)
-    app.send_message(chat_dest, msg)
+logger = logging.getLogger(__name__)
 
 
-if __name__ == '__main__':
-    app.config['api_key'] = environ.get('API_KEY')
-    app.poll(debug=True)
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except Unauthorized as e:
+        logger.error(e)
+        # remove update.message.chat_id from conversation list
+    except BadRequest as e:
+        logger.error(e)
+        # handle malformed requests - read more below!
+    except TimedOut as e:
+        logger.error(e)
+    # handle slow connection problems
+    except NetworkError as e:
+        logger.error(e)
+    # handle other connection problems
+    except ChatMigrated as e:
+        logger.error(e)
+    # the chat_id of a group has changed, use e.new_chat_id instead
+    except TelegramError as e:
+        logger.error(e)
+
+
+# handle all other telegram related errors
+
+dispatcher.add_error_handler(error_callback)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
